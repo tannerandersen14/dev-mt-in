cdd756cc02d253b4dcbb3110d23e45dc2d3ddc6e
Tanner Andersen <tanner.andersen14@gmail.com>
2016-02-15 16:44:13 -0800

    Day one.

---
+++ js/homeCtrl.js
@@ -0,0 +1,19 @@
+var app = angular.module("devMtIn");
+.controller('homeCtrl', function($scope) {
+  $scope.myProfile = {
+
+    friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+  }
+  $scope.sortOptions = [{
+    display: 'Ascending',
+    value: false
+  },
+  {
+    display: 'Descending',
+    value: true
+  }]
+
+
+
+
+})
